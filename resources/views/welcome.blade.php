610bb5e2e32d8dfd3eb89ecbbe00bd7e4b74cfa0
maharjanprogress <progressmaharjan1@gmail.com>
2025-09-06 18:50:36 +0545

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,12 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Laravel Progress</title>
+</head>
+<body>
+    <h1>Welcome to Laravel!</h1>
+    <p>This is a simple Laravel application.</p>
+    <a href="" class="btn">Test</a>
+</body>
+</html>
