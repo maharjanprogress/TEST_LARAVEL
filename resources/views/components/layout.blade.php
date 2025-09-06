d0a9b6578ffd395d53792080aef8a1fb35620318
maharjanprogress <progressmaharjan1@gmail.com>
2025-09-06 22:51:56 +0545

    database migration and rollbacks

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -3,6 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Base Layout</title>
+    @vite('resources/css/app.css')
 </head>
 <body>
     <header>
